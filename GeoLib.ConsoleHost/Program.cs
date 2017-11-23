9a0db02bd1ada51fca2a0c6f65d0a84928717bf2
kristina.p.georgieva <kristina.p.georgieva@gmail.com>
2017-11-23 17:52:00 +0200

    Configured console self hosting.

--- GeoLib.ConsoleHost/Program.cs
+++ GeoLib.ConsoleHost/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using GeoLib.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 
 namespace GeoLib.ConsoleHost
 {
@@ -8,11 +10,13 @@ namespace GeoLib.ConsoleHost
     {
         static void Main(string[] args)
         {
-
-
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+            hostGeoManager.Open();
+                
             Console.WriteLine("Services started. Press [Enter] to exit.");
             Console.ReadLine();
 
+            hostGeoManager.Close();
         }
     }
 }
